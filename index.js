8975d9ba797edb22d574c05b2cc0aa2c74e2a053
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-10-01 14:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function createRock(x) {
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock() {
+  function moveRock(timestamp) {
     // implement me!
     // (use the comments below to guide you!)
     /**
